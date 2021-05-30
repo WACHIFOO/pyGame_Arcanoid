b1947e4ba319a802b63c2c05053d66e42d7bf8b6
DanielGallegoRuiz <gallegoruizdaniel@gmail.com>
2021-05-31 01:13:10 +0200

    El personaje se mueve y esquinas de pantalla infinita

--- main.py
+++ main.py
@@ -1,16 +1,111 @@
-# This is a sample Python script.
+from classes.game import Game
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+# from classes.player import Player
+# from classes.sardina import Sardina
+# from classes.scoreUX import ScoreUX
+"""
+import pygame
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    game = Game()
+    game.flip()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    x = 250
+    y = 250
+    speed = 0
+    while game.playing:
+        # Controlamos si se pulsa cerrar
+        game.trigger_quit_game()
+
+        # Iniciamos el latido
+        game.start_hearthbeat()
+
+        # Test mover jugador
+        keys = pygame.key.get_pressed()
+
+        # Si hay movimiento aplicamos velocidad
+        if 1 in keys:
+            speed = 1.5
+        else:
+            if speed > 1.1:
+                speed -= 0.1
+
+        # Contrtolamos que tecla pulsa
+        if keys[pygame.K_LEFT]:
+            x -= 10
+        if keys[pygame.K_RIGHT]:
+            x += 10
+        if keys[pygame.K_UP]:
+            y -= 10
+        if keys[pygame.K_DOWN]:
+            y += 10
+
+        # if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
+        #     x -= 1
+        #     y -= 1
+        # elif keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
+        #     x -= 1
+        #     y += 1
+        # elif keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
+        #     x += 1
+        #     y -= 1
+        # elif keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
+        #     x += 1
+        #     y += 1
+
+        # Si llega a uno de los bordes que se vaya al otro lao
+        print(x)
+        # Derecha
+        if x >= 499:
+            x = 1
+        # Izquieda
+        if x <= 0:
+            x = 499
+        # Abajo
+        if y >= 499:
+            y = 1
+        # Arriba
+        if y <= 0:
+            y = 499
+
+        # print(str(x) + " - " + str(speed*x))
+        # print(str(y) + " - " + str(speed*y))
+        pygame.draw.circle(game.screen, (255, 255, 255), (x, y), 15)
+
+        # Finalizamos el latido
+        game.end_hearthbeat()
+    game.quit_game()
+
+    """
+    game = Game()
+    player = Player()
+    sardina = Sardina()
+    score = ScoreUX()
+    game.flip()
+    while game.playing:
+        # Controlamos si se pulsa cerrar
+        game.trigger_quit_game()
+
+        # Iniciamos el latido
+        game.start_hearthbeat()
+
+        # Obtenemos la tecla pulsada y movemos el jugador
+        pressed = pygame.key.get_pressed()
+        player.movement(pressed)
+
+        # Renderizamos el jugador
+        player.draw(game.screen)
+
+        # Controlamos si el jugador esta sobre la recompensa
+        sardina.colision(player.player_rect)
+        sardina.check_win()
+        sardina.blit(game.screen)
+
+        # Printamos la puntuacion
+        score.draw(game.screen, sardina.puntuacion)
+
+        # Finalizamos el latido
+        game.end_hearthbeat()
+    game.quit_game()
+"""
