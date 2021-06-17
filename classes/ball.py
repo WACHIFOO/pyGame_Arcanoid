2ce7561d9d7af645336123a002fdde29a9d6fa98
DanielGallegoRuiz <gallegoruizdaniel@gmail.com>
2021-06-17 22:04:54 +0200

    Pelota que rebota y colisiona con el jugador

---
+++ classes/ball.py
@@ -0,0 +1,90 @@
+import random
+
+import pygame
+from random import randint
+
+
+class Ball:
+    def __init__(self):
+        self.force = 5  # Fuerza para el movimiento
+        self.ball = pygame.image.load("sprites/Sardine.png")
+
+        # Tema posicionamineto en pantalla
+        self.ball_rect = self.ball.get_rect()
+        self.ball_rect.x = 250
+        self.ball_rect.y = 250
+
+        # Rangos maximos de movimiento por pantalla
+        self.max_derecha = pygame.display.get_window_size()[0] - self.ball.get_size()[0]
+        self.max_izquierda = 0
+        self.max_arriba = 0
+        self.max_abajo = pygame.display.get_window_size()[1] - self.ball.get_size()[1]
+
+        # variables para saber hacia donde va
+        self.cae = randint(0, 1)
+        self.bota = randint(0, 1)
+        self.fuerzaBote = random.uniform(1, 1.3)
+
+    def draw(self, screen):
+        screen.blit(self.ball, self.ball_rect)
+
+    def __check_limits(self):
+        """
+        Controlamos que no se pase del ux
+        """
+        if self.ball_rect.x > self.max_derecha:
+            self.ball_rect.x = self.max_derecha
+        elif self.ball_rect.x < 0:
+            self.ball_rect.x = 0
+
+    def movement(self):
+        """
+        Movemos la bola de manera random en el eje X y ponemos en negativo el eje Y
+        :param key_pressed: pygame.key.get_pressed()
+        """
+        self.__caer()
+        self.__botar()
+
+    def __caer(self):
+        """
+        Movemos la bola para arriba o para abajo
+        :return:
+        """
+        if self.cae:
+            self.ball_rect.y += self.force
+            # Si llega abajo cambiamos la fuerza para que rebote
+            if self.ball_rect.y == 480:
+                self.cae = False
+                self.bota = randint(0, 1)
+                self.fuerzaBote = random.uniform(1, 1.3)
+        else:
+            self.ball_rect.y -= self.force
+            # Si llega abajo cambiamos la fuerza para que rebote
+            if self.ball_rect.y == 20:
+                self.cae = True
+                self.bota = randint(0, 1)
+                self.fuerzaBote = random.uniform(1, 1.3)
+
+    def __botar(self):
+        """
+        Movemos la bola izquierda o derecha
+        :return:
+        """
+        # Aplicamos fuerza a la izquierda o a la derecha
+        fuerza_calculada = self.force * self.fuerzaBote
+        if self.bota:
+            self.ball_rect.x += fuerza_calculada
+        else:
+            self.ball_rect.x -= fuerza_calculada
+
+        #  Controlamos si rebota horizontalmente
+        control_horizontal = self.ball_rect.x < self.max_izquierda or self.ball_rect.x > self.max_derecha
+
+        if control_horizontal:
+            self.bota = randint(0, 1)
+            self.fuerzaBote = random.uniform(1, 1.3)
+            # Hacemos que no se bugee en las esquinas
+            if self.ball_rect.x < self.max_izquierda:
+                self.ball_rect.x = self.max_izquierda
+            elif self.ball_rect.x > self.max_derecha:
+                self.ball_rect.x = self.max_derecha
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from classes.game import Game
 from classes.player import Player
+from classes.ball import Ball
 
 """
 # from classes.sardina import Sardina
@@ -10,8 +11,9 @@ import pygame
 if __name__ == '__main__':
     game = Game()
     player = Player()
-    game.flip()
+    ball = Ball()
 
+    game.flip()
     x = 250
     y = 250
     speed = 0
@@ -26,94 +28,23 @@ if __name__ == '__main__':
         pressed = pygame.key.get_pressed()
         player.movement(pressed)
 
+        # Movemos la Bola
+        ball.movement()
+
+        # Checkeamos que la bola y el jugador se esten tocando
+        collide = player.player_rect.colliderect(ball.ball_rect)
+
+        if collide:
+            ball.cae = False
+            ball.movement()
+
         # Renderizamos el jugador
         player.draw(game.screen)
 
-        # # Test mover jugador
-        # keys = pygame.key.get_pressed()
-        #
-        # # Si hay movimiento aplicamos velocidad
-        # if 1 in keys:
-        #     speed = 1.5
-        # else:
-        #     if speed > 1.1:
-        #         speed -= 0.1
-
-        # # Contrtolamos que tecla pulsa
-        # if keys[pygame.K_LEFT]:
-        #     x -= 10
-        # if keys[pygame.K_RIGHT]:
-        #     x += 10
-        # if keys[pygame.K_UP]:
-        #     y -= 10
-        # if keys[pygame.K_DOWN]:
-        #     y += 10
-        #
-        # # if keys[pygame.K_LEFT] and keys[pygame.K_UP]:
-        # #     x -= 1
-        # #     y -= 1
-        # # elif keys[pygame.K_LEFT] and keys[pygame.K_DOWN]:
-        # #     x -= 1
-        # #     y += 1
-        # # elif keys[pygame.K_RIGHT] and keys[pygame.K_UP]:
-        # #     x += 1
-        # #     y -= 1
-        # # elif keys[pygame.K_RIGHT] and keys[pygame.K_DOWN]:
-        # #     x += 1
-        # #     y += 1
-        #
-        # # Si llega a uno de los bordes que se vaya al otro lao
-        # print(x)
-        # # Derecha
-        # if x >= 499:
-        #     x = 1
-        # # Izquieda
-        # if x <= 0:
-        #     x = 499
-        # # Abajo
-        # if y >= 499:
-        #     y = 1
-        # # Arriba
-        # if y <= 0:
-        #     y = 499
-        #
-        # # print(str(x) + " - " + str(speed*x))
-        # # print(str(y) + " - " + str(speed*y))
-        # pygame.draw.circle(game.screen, (255, 255, 255), (x, y), 15)
+        # Renderizamos la bola
+        ball.draw(game.screen)
 
         # Finalizamos el latido
         game.end_hearthbeat()
     game.quit_game()
 
-    """
-    game = Game()
-    player = Player()
-    sardina = Sardina()
-    score = ScoreUX()
-    game.flip()
-    while game.playing:
-        # Controlamos si se pulsa cerrar
-        game.trigger_quit_game()
-
-        # Iniciamos el latido
-        game.start_hearthbeat()
-
-        # Obtenemos la tecla pulsada y movemos el jugador
-        pressed = pygame.key.get_pressed()
-        player.movement(pressed)
-
-        # Renderizamos el jugador
-        player.draw(game.screen)
-
-        # Controlamos si el jugador esta sobre la recompensa
-        sardina.colision(player.player_rect)
-        sardina.check_win()
-        sardina.blit(game.screen)
-
-        # Printamos la puntuacion
-        score.draw(game.screen, sardina.puntuacion)
-
-        # Finalizamos el latido
-        game.end_hearthbeat()
-    game.quit_game()
-"""
